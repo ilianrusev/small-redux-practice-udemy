86df19d8b60fd38672d15ba0a951c24d65f7ae06
Iliyan Rusev <ilianrusev@gmail.com>
2022-05-20 16:33:05 +0300

    ui store made

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import uiSlice from './slices/ui-slice'
+
+
+const store = configureStore({
+    reducer: {
+        ui: uiSlice.reducer,
+
+    }
+})
+
+export default store;
