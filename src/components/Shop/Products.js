71f2e343777b2304bad85714941e13efff0155d6
Iliyan Rusev <ilianrusev@gmail.com>
2022-05-20 16:58:53 +0300

    added dummy products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,38 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+    {
+        id: 'p1',
+        price: 6,
+        title: 'Redux practice',
+        description: 'First practice of redux'
+    },
+    {
+        id: 'p2',
+        price: 7.5,
+        title: 'Redux practice 2',
+        description: 'Second practice of redux'
+    },
+]
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map(product => (
+                    <ProductItem
+                        key={product.id}
+                        title={product.title}
+                        price={product.price}
+                        description={product.description}
+                    />
+                ))}
+
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
